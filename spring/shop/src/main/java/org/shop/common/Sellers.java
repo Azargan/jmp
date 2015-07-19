828669c6cebb020b0d7424c4e48daab991da2bb9
Aliaksei Vihuro <aliaksei_vihuro@epam.com>
2015-07-19 23:03:55 +0300

    Spring core solution

---
+++ spring/shop/src/main/java/org/shop/common/Sellers.java
@@ -0,0 +1,14 @@
+package org.shop.common;
+
+public final class Sellers {
+
+    public static final String AMAZON = "Amazon";
+    
+    public static final String SAMSUNG = "Samsung";
+
+    public static final String APPLE = "Apple";
+
+    private Sellers() {
+        super();
+    }
+}
